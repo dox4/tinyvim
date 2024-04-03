d2a340d3581553ce6a3bfa58ee2c0f49967b289a
dox4 <dox4@foxmail.com>
2024-04-03 20:38:05 +0800

    fix: remove format on save since file autosaved

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,36 +1,4 @@
--- auto save current buffer
 local augroup = vim.api.nvim_create_augroup("user_commands", { clear = true })
--- vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
---     group = augroup,
---     pattern = "*",
---     callback = function(ev)
---         local bufnum = ev.buf
---
---         -- buf not loaded
---         if vim.fn.bufloaded(bufnum) ~= 1 then
---             return
---         end
---
---         local is_modifiable = function()
---             return vim.fn.getbufvar(bufnum, "&modifiable") == 1
---         end
---         if is_modifiable() then
---             local bufname = vim.api.nvim_buf_get_name(bufnum)
---             -- maybe a plugin-defined but not an exactly condition
---             if bufname == "" then
---                 return
---             end
---             vim.api.nvim_buf_call(bufnum, function()
---                 -- format on auto save
---                 -- require("conform").format()
---                 vim.cmd("silent! write")
---             end)
---             local msg = ("buf %%%d: %s automatically saved at %s."):format(bufnum, bufname, vim.fn.strftime("%H:%M:%S"))
---             vim.notify(msg, vim.log.levels.INFO)
---         end
---     end,
--- })
-
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
     group = augroup,
     pattern = "term://*",
--- lua/plugins/configs/conform.lua
+++ lua/plugins/configs/conform.lua
@@ -5,11 +5,6 @@ require("conform").setup({
         sh = { "shfmt" },
         json = { "prettier" },
     },
-    format_on_save = {
-        -- I recommend these options. See :help conform.format for details.
-        lsp_fallback = true,
-        timeout_ms = 500,
-    },
     formatters = {
         prettier = {
             inherit = true,
