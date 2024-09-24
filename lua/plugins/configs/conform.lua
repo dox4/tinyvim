690f5f2b6726aac1e2dd94da0de7e52c6e76e2b5
dox4 <dox4@foxmail.com>
2024-09-24 23:08:48 +0800

    feat: add prettier config

--- lua/plugins/configs/conform.lua
+++ lua/plugins/configs/conform.lua
@@ -6,12 +6,22 @@ require("conform").setup({
         json = { "prettier" },
         jsonc = { "prettier" },
         vue = { "prettier" },
-        ts = { "prettier" },
     },
     formatters = {
         prettier = {
             inherit = true,
-            prepend_args = { "--tab-width", "4" },
+            prepend_args = function(_, ctx)
+                local cmd = "prettier --find-config-path " .. ctx.filename
+                local prettierrc = vim.fn.system(cmd)
+                if vim.v.shell_error then
+                    local function trim(s)
+                        return s:match("^%s*(.-)%s*$")
+                    end
+                    prettierrc = trim(prettierrc)
+                    return { "--config", prettierrc }
+                end
+                return { "--tab-width", "4" }
+            end,
         },
 
         golines = {
